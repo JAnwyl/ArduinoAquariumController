a8ab8b4283e552dfcfbc90b088056090d0f0c2c4
Jared <jaredanwyl@gmail.com>
2013-12-10 03:38:08 -0800

    Update DHT11.ino

--- Components/DHT11/DHT11.ino
+++ Components/DHT11/DHT11.ino
@@ -1,10 +1,31 @@
-/*
-Jared Anwyl
-*/
-void setup() {
+Modified by Jared Anwyl
+#include <dht.h>
 
-}
+//Set analog pin here
+#define dht_dpin A0 //no ; here. Set equal to channel sensor is on
 
-void loop() {
+dht DHT;
 
-}
+void setup(){
+Serial.begin(9600);
+delay(300);//Let system settle
+Serial.println("Temperature\n");
+delay(700);//Wait rest of 1000ms recommended delay before
+//accessing sensor
+}//end "setup()"
+
+void loop(){
+//This is the "heart" of the program.
+DHT.read11(dht_dpin);
+
+Serial.print("temperature = ");
+Serial.print(DHT.temperature);
+Serial.print("C or ");
+float fahrenheit = (((DHT.temperature * 9)/5)+32);
+Serial.print(fahrenheit);
+Serial.println("F ");
+delay(800);//Don't try to access too frequently... in theory
+//should be once per two seconds, fastest,
+//but seems to work after 0.8 second.
+}// end loop()
+
