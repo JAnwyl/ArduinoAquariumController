ac5ce7fd2d44bdec4af30f0bdcb6916ea62cff53
JAnwyl <jaredanwyl@gmail.com>
2013-12-13 04:00:37 -0800

    Added Libraries

--- Components/LCD/LCD.ino
+++ Components/LCD/LCD.ino
@@ -1,10 +1,18 @@
-/*
-Jared Anwyl
-*/
-void setup() {
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
+//Addr: 0x3F, 20 chars & 4 lines
+//0x27 worked for other poster
+LiquidCrystal_I2C lcd(0x3F); 
+
+void setup()
+{
+    lcd.begin(20,4);               // initialize the lcd 
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello");
 }
-
-void loop() {
+void loop()
+{
 
 }
