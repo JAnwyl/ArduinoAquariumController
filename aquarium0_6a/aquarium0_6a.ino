dc570e2aef976d26ab011d339d5392e84325bb52
JAnwyl <jaredanwyl@gmail.com>
2013-12-25 02:19:20 -0800

    Progress

--- aquarium0_6a/aquarium0_6a.ino
+++ aquarium0_6a/aquarium0_6a.ino
@@ -26,14 +26,19 @@ This code is public domain
 ////////////////////////////////////Libraries///////////////////////////////////////
 ///////////////////////////////////#LIBRARIES///////////////////////////////////////
 #include <dht.h> //dht
-#include <Wire.h> //LCD
+#include <Wire.h> //LCD & Menu
 #include <LiquidCrystal_I2C.h> //LCD
 #include <OneWire.h> //DS18B20
 #include <DallasTemperature.h> //DS18B20
 #include <DS1302.h> //RTC
+#include <buttons.h> //Menu
+#include <EEPROM.h> //Menu
+#include <MENWIZ.h> //Menu
 
 ////////////////////////////////Global Variables////////////////////////////////////
 /////////////////////////////////////#VAR///////////////////////////////////////////
+const int debounceTime = 20; // number of milliseconds for switch to be stable
+
 double displayDTTempC, displayDTTempF;
 double displaySumpTempC, displaySumpTempF;
 double displayHoodTempC, displayHoodTempF;
@@ -42,6 +47,12 @@ double currentTime;
 boolean waveMaker;
 boolean feeding;
 String AmPm;
+//Menu Below
+int      tp=0;
+float    f=26.0;
+boolean  bb=0;
+byte     b=50;
+//Menu ^
 
 /////////////////////////////////////Define Pins////////////////////////////////////
 ///////////////////////////////////////#PIN/////////////////////////////////////////
@@ -68,7 +79,11 @@ const int skimmerHi= 25;
 //const int blueIn = ;
 //const int blueOut = ;
 //const int IR = ;
-const int debounceTime = 20; // number of milliseconds for switch to be stable
+// DEFINE ARDUINO PINS FOR THE NAVIGATION BUTTONS
+#define UP_BUTTON_PIN       9
+#define DOWN_BUTTON_PIN     10
+#define CONFIRM_BUTTON_PIN  12
+#define ESCAPE_BUTTON_PIN   11
 
 //Analog Pins
 #define dht_dpin A0
@@ -76,7 +91,7 @@ const int debounceTime = 20; // number of milliseconds for switch to be stable
 const int lm35 = 22;
 
 dht DHT;
-
+menwiz menu;
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
@@ -100,11 +115,14 @@ void setup() {
   lcd.setCursor(0, 1);
   lcd.print("Controller");
   sensors.begin();               // DS18B20
+  _menu *r,*s1,*s2;              //Menu
+  // inizialize the menu object (20 colums x 4 rows)
+  menu.begin(&lcd,20,4);
   
   Serial.begin(9600); 
    pinMode(uvPumpRelay, OUTPUT);//Relay
    pinMode(uvLightRelay, OUTPUT);//Relay
-   pinMode(returnPump, OUTPUT);//Relay
+   pinMode(returnPump, OUTPUT);//Relay  
    pinMode(proteinSkimmer, OUTPUT);//Relay
    pinMode(displayPumpLeft, OUTPUT);//Relay
    pinMode(displayPumpRight, OUTPUT);//Relay
@@ -120,14 +138,57 @@ void setup() {
    //pinMode(blueOut, OUTPUT);//BlueTooth
    //pinMode(IR, INPUT);//IR
    
+   //create the menu tree
+  r=menu.addMenu(MW_ROOT,NULL,F("MAIN MENU"));              //create a root menu at first (required)
+    s1=menu.addMenu(MW_SUBMENU,r,F("MEASURE SUBMENU"));     //add a child (submenu) node to the root menu
+    //s1->setBehaviour(MW_MENU_COLLAPSED,true);          
+    
+      s2=menu.addMenu(MW_VAR,s1,F("list"));            //add a terminal node (that is "variable"); 
+          s2->addVar(MW_LIST,&tp);                          //create a variable of type "option list".. 
+          s2->addItem(MW_LIST,F("option 1"));               //add option to the OPTION LIST
+          s2->addItem(MW_LIST,F("option 2"));               //add option to the OPTION LIST
+          s2->addItem(MW_LIST,F("option 3"));               //add option to the OPTION LIST
+          s2->addItem(MW_LIST,F("option 4"));               //add option to the OPTION LIST
+          s2->addItem(MW_LIST,F("option 5"));               //add option to the OPTION LIST
+//          s2->setBehaviour(MW_SCROLL_HORIZONTAL,true);    
+//          s2->setBehaviour(MW_LIST_2COLUMNS,true);          
+//          s2->setBehaviour(MW_LIST_3COLUMNS,true);          
+
+      s2=menu.addMenu(MW_VAR,s1,F("float var"));       //add a terminal node (that is "variable"); 
+          s2->addVar(MW_AUTO_FLOAT,&f,11.00,100.00,0.5); //create a variable of type "float number"... 
+                                                         //...associated to the terminal node and bind it to the app variable "f" of type float
+      s2=menu.addMenu(MW_VAR,s1,F("byte var"));        //add a terminal node (that is "variable"); 
+          s2->addVar(MW_AUTO_BYTE,&b,25,254,10);         //create a variable of type "byte"...
+                                                         //...associated to the terminal node and bind it to the app variable "b" of typr byte
+      s2=menu.addMenu(MW_VAR,s1,F("boolean var"));     //add a terminal node (that is "variable"); 
+          s2->addVar(MW_BOOLEAN,&bb);                    //create a variable of type "boolean" 
+                                                         //...associated to the terminal node and bind it to the app variable "bb" of type boolean
+    s1=menu.addMenu(MW_VAR,r,F("WRITE TO SERIAL"));             //add a terminal node (that is "variable") create an "action" associated to the terminal node... 
+      s1->addVar(MW_ACTION,act);                         //the act function as default will be called when enter button is pushed
+      s1->setBehaviour(MW_ACTION_CONFIRM,false);         //...if you don't need action confirmation
+
+  //declare navigation buttons (required)
+  menu.navButtons(UP_BUTTON_PIN,DOWN_BUTTON_PIN,ESCAPE_BUTTON_PIN,CONFIRM_BUTTON_PIN);
+
+  //(optional)create a user define screen callback to activate after 10 secs (10.000 millis) since last button push 
+  menu.addUsrScreen(msc,10000);
+
+  //(optional) create a splash screen (duration 5.000 millis)with some usefull infos the character \n marks end of LCD line 
+  //(tip): use preallocated internal menu.sbuf buffer to save memory space!
+//  sprintf(menu.sbuf,"MENWIZ TEST V %s\n.Free mem. :%d\n.Used mem  :%d\n.Lap secs  :%d",menu.getVer(),menu.freeRam(),mem-menu.freeRam(),5);
+//  menu.addSplash((char *) menu.sbuf, 5000);
 }
 
 /////////////////////////////////////Loop///////////////////////////////////////////
 /////////////////////////////////////#LOOP//////////////////////////////////////////
-
+void act(){
+  Serial.println("FIRED ACTION!");
+}
+ 
 void loop() {
   DHT.read11(dht_dpin);
   sensors.requestTemperatures();//DS18B20
+  menu.draw(); 
   
 /*  
     switch () {
@@ -156,6 +217,15 @@ void loop() {
 
 /////////////////////////////////////Methods////////////////////////////////////////
 /////////////////////////////////////#METHODS///////////////////////////////////////
+void msc(){
+  static  char buf[7];
+  strcpy(menu.sbuf,"User screen"); //1st lcd line
+  strcat(menu.sbuf,"\nUptime (s): ");strcat(menu.sbuf,itoa((int)(millis()/1000),buf,10));//2nd lcd line
+  strcat(menu.sbuf,"\nFree mem  : ");strcat(menu.sbuf,itoa((int)menu.freeRam(),buf,10));//3rd lcd line
+  strcat(menu.sbuf,"\n"); //4th lcd line (empty)
+  menu.drawUsrScreen(menu.sbuf);
+}
+ 
 void temp(){
   //Controls Heater
   //This isn't right
